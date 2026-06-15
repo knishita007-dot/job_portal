deaa7f9ca5cc9aaec2552faefed87354d3ebaff7
knishita007-dot <knishita007@gmail.com>
2026-06-15 18:09:46 +0530

    Fix JDBC connection prefix in JobPortalApplication main method

--- springboot-service/src/main/java/com/jobportal/JobPortalApplication.java
+++ springboot-service/src/main/java/com/jobportal/JobPortalApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobPortalApplication {
     public static void main(String[] args) {
+        String dbUrl = System.getenv("SPRING_DATASOURCE_URL");
+        if (dbUrl != null && dbUrl.startsWith("postgresql://")) {
+            System.setProperty("SPRING_DATASOURCE_URL", "jdbc:" + dbUrl);
+        }
         SpringApplication.run(JobPortalApplication.class, args);
     }
 }
